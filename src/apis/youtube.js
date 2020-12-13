debabc8bcba05850a7329c54b2055406ee57d277
LeeTrent <lee.trent.1@gmail.com>
2020-12-13 12:30:13 -0600

    Removes API key value

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const KEY = 'AIzaSyBrThlSsr1oMxlrOh7EKn5Ks3YgQxznjFg';
+const KEY = '';
 
 export default axios.create({
     baseURL: 'https:///www.googleapis.com/youtube/v3',
